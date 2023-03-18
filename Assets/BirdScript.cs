b52c9775db95b4de249fe8990e647be8ae859c38
JZZWW <116762754+JZZWW@users.noreply.github.com>
2023-03-18 15:37:48 +0100

    First Commit

---
+++ Assets/BirdScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    public Rigidbody2D BirdBody; 
+    public float flapStrength; 
+    public LogicScript logic;
+    public bool isAlive = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isAlive) {
+            BirdBody.velocity = Vector2.up * flapStrength;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        logic.gameOver();
+        isAlive = false;
+    }
+}
